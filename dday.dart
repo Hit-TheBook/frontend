5533d9e44fd36ffe36ad47ca66d5c2e1a68b731a
공영준 <01062542501o@gmail.com>
2024-08-19 22:31:26 +0900

    Add files via upload

--- dday.dart
+++ dday.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:project1/ddaydetail.dart';
 import 'package:project1/theme.dart';
 import 'count_up_timer_page.dart';
@@ -14,35 +14,254 @@ class DdayPage extends StatefulWidget {
 }
 
 class DdayPageState extends State<DdayPage> {
+  final List<Map<String, dynamic>> _ddayList = []; // 디데이 정보를 저장할 리스트
+  Map<String, dynamic>? _selectedDday; // 현재 선택된 디데이 정보
+
+  void _navigateToDdayDetail() async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const DdaydetailPage()),
+    );
+
+    if (result != null) {
+      setState(() {
+        _ddayList.add(result);
+      });
+    }
+  }
+
+  void _showDdayOptions(BuildContext context, Map<String, dynamic> dday) {
+    _selectedDday = dday;
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: const Color(0xFF333333), // 배경색 설정
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0), // 둥근 테두리
+          ),
+          contentPadding: EdgeInsets.zero, // Padding 제거
+          content: Container(
+            width: MediaQuery.of(context).size.width * 0.8, // 화면 가로의 80%로 설정
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                ListTile(
+                  title: const Text(
+                    '대표 디데이 설정',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white), // 글씨색 흰색
+                  ),
+                  onTap: () {
+                    Navigator.pop(context); // 다이얼로그 닫기
+                    _setAsFeaturedDday(dday);
+                  },
+                ),
+                ListTile(
+                  title: const Text(
+                    '디데이 정보 수정',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white), // 글씨색 흰색
+                  ),
+                  onTap: () {
+                    Navigator.pop(context); // 다이얼로그 닫기
+                    _editDdayInfo(dday);
+                  },
+                ),
+                ListTile(
+                  title: const Text(
+                    '디데이 삭제',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white), // 글씨색 흰색
+                  ),
+                  onTap: () {
+                    Navigator.pop(context); // 다이얼로그 닫기
+                    _deleteDday(dday);
+                  },
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void _setAsFeaturedDday(Map<String, dynamic> dday) {
+    setState(() {
+      // 디데이를 리스트의 가장 위로 이동시킵니다.
+      _ddayList.remove(dday);
+      _ddayList.insert(0, dday);
+    });
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('${dday['title']}이(가) 대표 디데이로 설정되었습니다.')),
+    );
+  }
+
+  void _editDdayInfo(Map<String, dynamic> dday) async {
+    // 선택한 디데이 정보를 수정하기 위해 DdaydetailPage를 열고 결과를 받습니다.
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => DdaydetailPage(
+          initialTitle: dday['title'],
+          initialStartDate: dday['startDate'],
+          initialEndDate: dday['endDate'],
+        ),
+      ),
+    );
+
+    if (result != null) {
+      setState(() {
+        // 기존 디데이 정보를 수정합니다.
+        final index = _ddayList.indexOf(dday);
+        if (index != -1) {
+          _ddayList[index] = result;
+        }
+      });
+    }
+  }
+
+  void _deleteDday(Map<String, dynamic> dday) {
+    setState(() {
+      _ddayList.remove(dday);
+    });
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('${dday['title']}이(가) 삭제되었습니다.')),
+    );
+  }
+
+  String _formatDate(DateTime date) {
+    return DateFormat('yyyy.MM.dd').format(date);
+  }
+
+  String _calculateDuration(DateTime startDate, DateTime endDate) {
+    final difference = endDate.difference(startDate).inDays;
+    return '$difference일';
+  }
+
+  String? _calculateDaysUntilStart(DateTime startDate) {
+    final today = DateTime.now();
+    final difference = startDate.difference(today).inDays;
+    if (difference > 0) {
+      return '$difference일 남음';
+    }
+    return null; // 오늘 날짜가 시작일을 지나면 null 반환
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBar(
-        title: '디데이 ', // 사용할 타이틀을 전달
+        title: '디데이', // 사용할 타이틀을 전달
       ),
-      body: const Center(
-        child: Column(
-          // 여기에 위젯을 추가할 수 있습니다.
+      body: _ddayList.isEmpty
+          ? const Center(
+        child: Text(
+          '디데이를 추가해 보세요',
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 18,
+          ),
         ),
+      )
+          : ListView.builder(
+        itemCount: _ddayList.length,
+        itemBuilder: (context, index) {
+          final dday = _ddayList[index];
+          final title = dday['title'] ?? '디데이 제목';
+          final startDate = dday['startDate'] != null
+              ? DateTime.parse(dday['startDate'].toString())
+              : null;
+          final endDate = dday['endDate'] != null
+              ? DateTime.parse(dday['endDate'].toString())
+              : null;
+          final formattedStartDate = startDate != null
+              ? _formatDate(startDate)
+              : '시작일 없음';
+          final formattedEndDate = endDate != null
+              ? _formatDate(endDate)
+              : '종료일 없음';
+          final duration = (startDate != null && endDate != null)
+              ? _calculateDuration(startDate, endDate)
+              : '';
+          final daysUntilStart = (startDate != null)
+              ? _calculateDaysUntilStart(startDate)
+              : null;
+
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xFF333333), // 배경색
+                borderRadius: BorderRadius.circular(16.0), // 둥근 테두리
+              ),
+              child: ElevatedButton(
+                onPressed: () => _showDdayOptions(context, dday),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent, // 배경색 투명
+                  elevation: 0, // 그림자 제거
+                  minimumSize: const Size(double.infinity, 60), // 높이 조정
+                  padding: EdgeInsets.zero, // Padding 제거
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0), // 여백 추가
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            title,
+                            textAlign: TextAlign.left,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold, // 글씨 굵게
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            '$formattedStartDate~$formattedEndDate ($duration)',
+                            textAlign: TextAlign.left,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ],
+                      ),
+                      if (daysUntilStart != null) // 오늘 날짜가 시작일을 지나지 않았다면
+                        Text(
+                          daysUntilStart,
+                          textAlign: TextAlign.right,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // 버튼 클릭 시 동작을 여기에 추가
-          if (kDebugMode) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const DdaydetailPage()),
-            );
-          }
-        },
-        backgroundColor: AppColors.primary, // + 아이콘
-        elevation: 6.0, // 버튼의 그림자 설정
+        onPressed: _navigateToDdayDetail,
+        backgroundColor: AppColors.primary,
+        elevation: 6.0,
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(100), // 원형 버튼
-        ), // 아이콘 배경색 설정
+          borderRadius: BorderRadius.circular(100),
+        ),
         child: const Icon(Icons.add),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat, // FAB 위치를 오른쪽 하단으로 설정
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
--- ddaydetail.dart
+++ ddaydetail.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:project1/theme.dart'; // 테마 사용
-import 'count_up_timer_page.dart';
-import 'custom_appbar.dart'; // 기존 CustomAppBar
+import 'package:flutter/cupertino.dart';
+import 'package:intl/intl.dart'; // 날짜 포맷을 위해 필요한 패키지
 
 class DdaydetailPage extends StatefulWidget {
-  const DdaydetailPage({super.key});
+  final String? initialTitle;
+  final DateTime? initialStartDate;
+  final DateTime? initialEndDate;
+
+  const DdaydetailPage({
+    super.key,
+    this.initialTitle,
+    this.initialStartDate,
+    this.initialEndDate,
+  });
 
   @override
   DdaydetailPageState createState() => DdaydetailPageState();
@@ -13,6 +21,17 @@ class DdaydetailPage extends StatefulWidget {
 
 class DdaydetailPageState extends State<DdaydetailPage> {
   final TextEditingController _titleController = TextEditingController();
+  DateTime? _selectedStartDate; // 선택된 시작일을 저장할 변수
+  DateTime? _selectedEndDate; // 선택된 종료일을 저장할 변수
+
+  @override
+  void initState() {
+    super.initState();
+    _titleController.text = widget.initialTitle ?? '';
+    // 오늘 날짜로 기본값 설정
+    _selectedStartDate = widget.initialStartDate ?? DateTime.now();
+    _selectedEndDate = widget.initialEndDate;
+  }
 
   @override
   void dispose() {
@@ -20,18 +39,101 @@ class DdaydetailPageState extends State<DdaydetailPage> {
     super.dispose();
   }
 
-  void _startDate() {
-    // 시작일 버튼 클릭 시 동작 추가
+  void _showDatePicker({required bool isStartDate}) {
+    DateTime? minimumDate;
+    DateTime? initialDate = DateTime.now();
+
+    // 시작일이 설정된 경우 종료일 선택 시 최소 날짜 설정
+    if (!isStartDate && _selectedStartDate != null) {
+      minimumDate = _selectedStartDate;
+      initialDate = _selectedEndDate ?? minimumDate;
+    }
+
+    showCupertinoModalPopup(
+      context: context,
+      builder: (_) => Container(
+        height: 250,
+        color: Colors.white,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: SizedBox(
+                height: 160,
+                child: CupertinoDatePicker(
+                  initialDateTime: initialDate,
+                  mode: CupertinoDatePickerMode.date,
+                  minimumDate: minimumDate,
+                  onDateTimeChanged: (DateTime newDate) {
+                    setState(() {
+                      if (isStartDate) {
+                        _selectedStartDate = newDate;
+                        if (_selectedEndDate != null && newDate.isAfter(_selectedEndDate!)) {
+                          _selectedEndDate = null; // 시작일이 종료일 이후로 변경된 경우 종료일을 초기화
+                        }
+                      } else {
+                        if (newDate.isBefore(_selectedStartDate ?? newDate)) {
+                          // 종료일이 시작일 이전으로 선택된 경우
+                          return;
+                        }
+                        _selectedEndDate = newDate;
+                      }
+                    });
+                  },
+                ),
+              ),
+            ),
+            CupertinoButton(
+              child: const Text('Done'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  String _formatDate(DateTime? date) {
+    if (date == null) return '';
+    return DateFormat('yyyy-MM-dd').format(date);
+  }
+
+  void _selectStartDate() {
+    _showDatePicker(isStartDate: true);
     if (kDebugMode) {
       print('Start Date button clicked');
     }
   }
 
-  void _endDate() {
-    // 종료일 버튼 클릭 시 동작 추가
+  void _selectEndDate() {
+    if (_selectedStartDate == null) {
+      // 시작일이 설정되지 않은 경우
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('먼저 시작일을 선택해주세요.')),
+      );
+      return;
+    }
+    _showDatePicker(isStartDate: false);
     if (kDebugMode) {
       print('End Date button clicked');
     }
+  }
+
+  void _saveDday() {
+    if (_titleController.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('디데이 이름을 입력해주세요.')),
+      );
+      return;
+    }
+
+    Navigator.of(context).pop({
+      'title': _titleController.text,
+      'startDate': _selectedStartDate,
+      'endDate': _selectedEndDate,
+    });
   }
 
   @override
@@ -39,87 +141,132 @@ class DdaydetailPageState extends State<DdaydetailPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('디데이 추가'),
-        centerTitle: true, // 타이틀을 중앙에 위치시킴
+        centerTitle: true,
         actions: <Widget>[
           TextButton(
-            onPressed: () {
-              // 완료 버튼 클릭 시 동작 추가
-              // 예: 데이터를 저장하거나 다른 페이지로 이동
-            },
+            onPressed: _saveDday,
             child: const Text(
               '완료',
               style: TextStyle(
-                color: Colors.white, // 버튼 텍스트 색상
-                fontWeight: FontWeight.bold, // 버튼 텍스트 두께
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
               ),
             ),
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch, // 양쪽 끝으로 확장
-          children: [
-            const Text(
-              '디데이 이름',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text(
+                '디데이 이름',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            const SizedBox(height: 8),
-            TextField(
-              controller: _titleController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: '디데이 이름을 입력하세요',
+              const SizedBox(height: 8),
+              TextField(
+                controller: _titleController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: '디데이 이름을 입력하세요',
+                  hintStyle: TextStyle(color: Colors.grey), // 힌트 텍스트 색상
+                ),
               ),
-            ),
-            const SizedBox(height: 16),
-            const Text(
-              '날짜',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
+              const SizedBox(height: 16),
+              const Text(
+                '날짜',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            const SizedBox(height: 8),
-            Column(
-              children: [
-                ElevatedButton(
-                  onPressed: _startDate,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).colorScheme.primary,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12), // 둥근 직사각형
-                    ),
-                    minimumSize: const Size(double.infinity, 48), // 버튼을 양옆으로 넓게
-                  ),
-                  child: const Text(
-                    '시작일',
-                    style: TextStyle(color: Colors.white), // 버튼 내부 텍스트 색상
-
+              const SizedBox(height: 8),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(
+                    color: Theme.of(context).colorScheme.primary,
                   ),
                 ),
-                const SizedBox(height: 8),
-                ElevatedButton(
-                  onPressed: _endDate,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).colorScheme.primary,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12), // 둥근 직사각형
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: _selectStartDate,
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context).colorScheme.primary,
+                              shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(12),
+                                  topRight: Radius.circular(12),
+                                ),
+                              ),
+                              minimumSize: const Size(double.infinity, 48),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text(
+                                  '시작일',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                Text(
+                                  _formatDate(_selectedStartDate),
+                                  style: const TextStyle(color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                    minimumSize: const Size(double.infinity, 48), // 버튼을 양옆으로 넓게
-                  ),
-                  child: const Text(
-                    '종료일',
-                    style: TextStyle(color: Colors.white), // 버튼 내부 텍스트 색상
-                  ),
+                    const Divider(height: 1, color: Colors.white),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: _selectEndDate,
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context).colorScheme.primary,
+                              shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(12),
+                                  bottomRight: Radius.circular(12),
+                                ),
+                              ),
+                              minimumSize: const Size(double.infinity, 48),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text(
+                                  '종료일',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                Text(
+                                  _formatDate(_selectedEndDate).isEmpty ? '종료일' : _formatDate(_selectedEndDate),
+                                  style: const TextStyle(color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ],
+              ),
+              const SizedBox(height: 16), // 여유 공간 추가
+            ],
+          ),
         ),
       ),
     );
