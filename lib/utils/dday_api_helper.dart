9aa15714a4969c51fe2923ff1029e4af85f7630f
공영준 <01062542501o@gmail.com>
2024-09-11 17:25:02 +0900

    Feat : api dday 통신기능구현중,디데이페이지 ui

--- lib/utils/dday_api_helper.dart
+++ lib/utils/dday_api_helper.dart
@@ -1,14 +1,15 @@
 import 'dart:convert';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
+import '../models/dday_model.dart'; // 모델 클래스를 import합니다.
 
 class ApiHelper {
   static const String baseUrl = 'http://13.209.78.125'; // 서버 URL
-  static const String temporaryToken = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0M0BleGFtcGxlLmNvbV85Mmlvc2RmOTNpc2Rmamkzb2kyMzRtb2ZzZGlqMiIsImlhdCI6MTcyNTk0MjA4MywiZXhwIjoxNzI1OTQ1NjgzfQ.vrGWoliWNvwtsaI6nBvYI6_NfuxN6oMUZThEw-75cFh7Qkq_IibQVjAHKGl8-NoPEZn_ZSC5nknaFovMs4aNAw'; // 서버에서 발급받은 임시 토큰
+  static const String temporaryToken = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0M0BleGFtcGxlLmNvbV85Mmlvc2RmOTNpc2Rmamkzb2kyMzRtb2ZzZGlqMiIsImlhdCI6MTcyNTk0NjQxOSwiZXhwIjoxNzU3NDgyNDE5fQ.t-LwL_f9huhSTzDMGLWLF_PAgqVq4NAk49kx1weMuFY1-eVY6OEBC1qm0rkmNyJAdIMylYtAuVq8Y8LS9IdUhQ'; // 서버에서 발급받은 임시 토큰
 
   // GET 요청
   static Future<http.Response> getRequest(String endpoint) async {
-    final url = Uri.parse('$baseUrl/dday/list');
+    final url = Uri.parse('$baseUrl/$endpoint');
     final response = await http.get(
       url,
       headers: {
@@ -16,7 +17,7 @@ class ApiHelper {
         'Content-Type': 'application/json',
       },
     );
-    debugPrint('Authorization header: Bearer $temporaryToken');
+
     debugPrint('GET $url');
     debugPrint('Response status: ${response.statusCode}');
     debugPrint('Response body: ${response.body}');
@@ -81,10 +82,10 @@ class ApiHelper {
   }
 
   // Dday 추가
-  static Future<Map<String, dynamic>> addDday(Map<String, dynamic> dday) async {
-    final response = await postRequest('dday', dday);
+  static Future<Map<String, dynamic>> addDday(RequestDday dday) async {
+    final response = await postRequest('dday', dday.toJson());
 
-    if (response.statusCode == 201) {
+    if (response.statusCode == 200) {
       return json.decode(response.body);
     } else {
       throw Exception('Failed to add Dday');
@@ -92,8 +93,8 @@ class ApiHelper {
   }
 
   // Dday 수정
-  static Future<Map<String, dynamic>> updateDday(String id, Map<String, dynamic> dday) async {
-    final response = await putRequest('dday/$id', dday);
+  static Future<Map<String, dynamic>> updateDday(String id, RequestDday dday) async {
+    final response = await putRequest('dday/$id', dday.toJson());
 
     if (response.statusCode == 200) {
       return json.decode(response.body);
