9c28e8c67b7da56fede72de266cfaa8e83735724
공영준 <01062542501o@gmail.com>
2024-11-13 17:13:03 +0900

    feat 윈시간표 테이블

--- lib/colors.dart
+++ lib/colors.dart
@@ -4,3 +4,4 @@ const neonskyblue1 = Color(0xff69EEFF);
 const black1 = Color(0xff000000);
 const white1 = Color(0xffffffff);
 const gray1 = Color(0xff333333);
+
--- lib/pages/time_circle_planner_page.dart
+++ lib/pages/time_circle_planner_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:project1/colors.dart';
 import 'package:project1/widgets/time_circle_planner_painter.dart';
 import 'package:project1/utils/planner_api_helper.dart';
 import 'dart:convert';
@@ -16,6 +16,17 @@ class TimeCirclePlannerPage extends StatefulWidget {
 class _TimeCirclePlannerPageState extends State<TimeCirclePlannerPage> {
   int colorIndex = 0;
   List<PlannerModel> schedules = [];
+
+  // TimeCirclePlannerPainter에 정의된 colors 리스트를 여기에 동일하게 정의
+  final List<Color> colors = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.blue,
+    Colors.indigo,
+    Colors.purple,
+  ];
 
   @override
   void initState() {
@@ -59,17 +70,130 @@ class _TimeCirclePlannerPageState extends State<TimeCirclePlannerPage> {
     ScreenUtil.init(context, designSize: const Size(375, 812), minTextAdapt: true);
 
     return Scaffold(
-      appBar: AppBar(title: const Text('원 시간표')),
-      body: SingleChildScrollView( // 스크롤 추가
-        child: Center(
+      appBar:  AppBar(title: Text('원 시간표')),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.w), // 화면 좌우 여백 추가
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              SizedBox(height: 20.h),
               CustomPaint(
                 size: Size(260.w, 260.w),
                 painter: TimeCirclePlannerPainter(schedules),
               ),
               SizedBox(height: 20.h),
+              schedules.isNotEmpty // 테이블 데이터가 있을 경우에만 테이블을 표시
+                  ? Theme(
+                data: Theme.of(context).copyWith(
+                  dividerColor: Colors.white,
+                ),
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10.w),
+                  child: DataTable(
+                    columnSpacing: 17.w,
+                    columns: [
+                      DataColumn(
+                        label: Padding(
+                          padding: EdgeInsets.only(top: 20.h),
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              '색상',
+                              style: TextStyle(color: neonskyblue1, fontSize: 12.sp),
+                            ),
+                          ),
+                        ),
+                      ),
+                      DataColumn(
+                        label: Padding(
+                          padding: EdgeInsets.only(top: 20.h),
+                          child: Container(
+                            width: 60.w,
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                '주제',
+                                style: TextStyle(color: neonskyblue1, fontSize: 12.sp),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      DataColumn(
+                        label: Padding(
+                          padding: EdgeInsets.only(top: 20.h),
+                          child: Container(
+                            width: 150.w,
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                '내용',
+                                style: TextStyle(color: neonskyblue1, fontSize: 12.sp),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                    rows: List<DataRow>.generate(
+                      schedules.length,
+                          (index) {
+                        final schedule = schedules[index];
+                        final color = colors[index % colors.length];
+
+                        return DataRow(
+                          cells: [
+                            DataCell(
+                              Center(
+                                child: Container(
+                                  width: 25.w,
+                                  height: 25.w,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: color,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            DataCell(
+                              Container(
+                                width: 60.w,
+                                child: Center(
+                                  child: Text(
+                                    schedule.scheduleTitle,
+                                    style: TextStyle(color: Colors.white, fontSize: 12.sp),
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 1,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            DataCell(
+                              Container(
+                                width: 150.w,
+                                child: Center(
+                                  child: Text(
+                                    schedule.content,
+                                    style: TextStyle(color: Colors.white, fontSize: 12.sp),
+                                    softWrap: true,
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 2,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              )
+                  : Container(
+                height: 200.h, // 테이블이 없을 때의 빈 공간 설정
+              ),
+              SizedBox(height: 300.h), // 하단 여백 추가
             ],
           ),
         ),
